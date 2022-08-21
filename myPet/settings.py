b3fb86b0ed5bf3e75b0f0dbfdb8cebde58f2d5cd
DESKTOP-UKQMQGV\BSM <blissman@kakao.com>
2022-08-21 14:15:21 +0900

    no message

--- myPet/settings.py
+++ myPet/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import datetime
+import date
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,8 +81,15 @@ WSGI_APPLICATION = 'myPet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'security',
+        'USER': 'admin',
+        'PASSWORD':'mypet56#$',
+        'HOST':'database-1.c52vilkgn02m.ap-northeast-2.rds.amazonaws.com',
+        'PORT':'3306',
+        'OPTIONS': {
+                'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
